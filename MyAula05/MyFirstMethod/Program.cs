18d83bfafd3cc9ee1e465f9ece417fe6b76c7ca1
LegendaryBananaCat02 <34323618+LegendaryBananaCat@users.noreply.github.com>
2019-03-29 12:22:56 +0000

    Add files via upload

--- MyAula05/MyFirstMethod/Program.cs
+++ MyAula05/MyFirstMethod/Program.cs
@@ -4,19 +4,60 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Counts to 10 twice
+        /// </summary>
+        /// <return></return>
         static void CountTo10()
         {
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine(i);
+                //Console.WriteLine(i);
+                CountToN(10);
             }
         }
+
+        /// <summary>
+        /// Counts from 1 to Max
+        /// </summary>
+        /// <param name="n"> Nº MAX to of count </param>
+        /// <return> Int count from 1 to n </return>
+        static void CountToN(int n)
+        {
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine(i);
+        }
+
+        /// <summary>
+        /// Calls fuctions.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             for (int i = 1; i < 3; i++)
             {
+                CountToN(8);
+                CountToN(2);
+                CountToN(2,4);
                 CountTo10();
             }
         }
+
+        /// <summary>
+        /// Shows nº between n1 & n2
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+
+        static void CountToN(int n1, int n2)
+        {
+            for (int i = 1; i <= n1; i++)
+                Console.WriteLine(i);
+
+            for (int j = 1; j <= n2; j++)
+                Console.WriteLine(j);
+        }
+
+        
     }
 }
---
+++ MyAula05/UnityTest/Assets/Scripts/Cubee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubee : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Hello stranger...");
+        transform.localScale = new Vector3(3, 5, 3);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 rot = new Vector3(0.1f, 0.5f, 0.1f);
+
+        transform.Rotate(rot);
+	}
+}
