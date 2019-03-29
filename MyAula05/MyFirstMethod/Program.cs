b50de703d74a3b16ea957cc62b9050363c55b39a
LegendaryBananaCat02 <34323618+LegendaryBananaCat@users.noreply.github.com>
2019-03-29 09:46:01 +0000

    Add files via upload

---
+++ MyAula05/MyFirstMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyFirstMethod
+{
+    class Program
+    {
+        static void CountTo10()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 3; i++)
+            {
+                CountTo10();
+            }
+        }
+    }
+}
